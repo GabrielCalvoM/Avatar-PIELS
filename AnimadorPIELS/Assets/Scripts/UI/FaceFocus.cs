47e880adcc6bdae7181a05cd5fddf4e740b8ea25
C-Espriella13 <cahucaes05@gmail.com>
2026-03-23 12:35:38 -0600

    Face UI 003

--- AnimadorPIELS/Assets/Scripts/UI/FaceFocus.cs
+++ AnimadorPIELS/Assets/Scripts/UI/FaceFocus.cs
@@ -1,13 +1,18 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class FaceFocus : MonoBehaviour
 {
+    [Header("Cameras")]
+    [SerializeField] private GameObject mainCamera;
+    [SerializeField] private GameObject faceCamera;
 
-    private bool presionado = false;
+    private bool isFocused = false;
 
     public void OnButtonPressed()
     {
-        
+        isFocused = !isFocused;
+
+        mainCamera.SetActive(!isFocused);
+        faceCamera.SetActive(isFocused);
     }
 }
