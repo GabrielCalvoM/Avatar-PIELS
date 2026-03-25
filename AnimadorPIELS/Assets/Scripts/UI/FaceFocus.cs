0b514a13c43c75f3759769d7b1f84e4becd87732
C-Espriella13 <cahucaes05@gmail.com>
2026-03-24 18:22:56 -0600

    Face UI 010 - Fini

--- AnimadorPIELS/Assets/Scripts/UI/FaceFocus.cs
+++ AnimadorPIELS/Assets/Scripts/UI/FaceFocus.cs
@@ -5,21 +5,33 @@ public class FaceFocus : MonoBehaviour
 {
     //////////////////////////////////////////////////////////// ATTRIBUTES
     
-    [Header ("UI Manager")]
+    [Header ("UI Components")]
     [SerializeField] private UIManager uiManager;
+    [SerializeField] private GameObject focusButton; 
+    private bool isFocused = false;
 
     [Header("Cameras")]
     [SerializeField] private GameObject mainCamera;
     [SerializeField] private GameObject faceCamera;
-    private bool isFocused = false;
 
     [Header("Avatar Face")]
     [SerializeField] private SkinnedMeshRenderer avatarFace;
+
     [SerializeField] private Slider leftEyelidSlider;
     [SerializeField] private Slider rightEyelidSlider;
-
     private int leftEyelidIndex;
     private int rightEyelidIndex;
+
+    [SerializeField] private Slider raiseEyebrowSlider;
+    [SerializeField] private Slider angleEyebrowSlider;
+    private int raiseEyebrowIndex;
+    private int low_angleEyebrowIndex;
+    private int high_angleEyebrowIndex;
+
+    [SerializeField] private Slider mouthHSlider;
+    [SerializeField] private Slider mouthVSlider;
+    private int mouthHIndex;
+    private int mouthVIndex;
 
     //////////////////////////////////////////////////////////// SIGNALS - CAMERA
 
@@ -30,6 +42,7 @@ public class FaceFocus : MonoBehaviour
         mainCamera.SetActive(!isFocused);
         faceCamera.SetActive(isFocused);
 
+        focusButton.SetActive(!isFocused);
         uiManager.RefreshUI();
     }
 
@@ -58,23 +71,84 @@ public class FaceFocus : MonoBehaviour
         avatarFace.SetBlendShapeWeight(rightEyelidIndex, value);
     }
 
+    private void OnRaiseEyebrowChanged(float value)
+    {
+        avatarFace.SetBlendShapeWeight(raiseEyebrowIndex, value);
+    }
+
+    private void OnAngleEyebrowChanged(float value)
+    {
+        if (Mathf.Approximately(value, 50.0f)) {
+            avatarFace.SetBlendShapeWeight(low_angleEyebrowIndex , 0.0f);
+            avatarFace.SetBlendShapeWeight(high_angleEyebrowIndex, 0.0f);
+        }
+
+        else if (value < 50.0f) {
+            float w = Mathf.InverseLerp(50.0f, 0.0f, value) * 100.0f;
+
+            avatarFace.SetBlendShapeWeight(low_angleEyebrowIndex , w);
+            avatarFace.SetBlendShapeWeight(high_angleEyebrowIndex, 0.0f);
+        }
+
+        else if (value > 50.0f) {
+            float w = Mathf.InverseLerp(50.0f, 100.0f, value) * 100.0f;
+
+            avatarFace.SetBlendShapeWeight(low_angleEyebrowIndex , 0.0f);
+            avatarFace.SetBlendShapeWeight(high_angleEyebrowIndex, w);
+        }
+    }
+
+    private void OnMouthHChanged(float value)
+    {
+        avatarFace.SetBlendShapeWeight(mouthHIndex, value);
+    }
+
+    private void OnMouthVChanged(float value)
+    {
+        avatarFace.SetBlendShapeWeight(mouthVIndex, value);
+    }
+
     //////////////////////////////////////////////////////////// GAME LOOP
     
     void Start()
     {
         // Get Idx
-        leftEyelidIndex = avatarFace.sharedMesh.GetBlendShapeIndex("blink_left");
-        rightEyelidIndex = avatarFace.sharedMesh.GetBlendShapeIndex("blink_right");
+        leftEyelidIndex  = avatarFace.sharedMesh.GetBlendShapeIndex("blink_left.001");
+        rightEyelidIndex = avatarFace.sharedMesh.GetBlendShapeIndex("blink_right.001");
+        
+        raiseEyebrowIndex = avatarFace.sharedMesh.GetBlendShapeIndex("raise_eyebrows3");
+        low_angleEyebrowIndex = avatarFace.sharedMesh.GetBlendShapeIndex("angry");
+        high_angleEyebrowIndex = avatarFace.sharedMesh.GetBlendShapeIndex("sad");
+
+        mouthHIndex = avatarFace.sharedMesh.GetBlendShapeIndex("open_mouth");
+        mouthVIndex = avatarFace.sharedMesh.GetBlendShapeIndex("o_mouth");
 
         // Set Range
-        leftEyelidSlider.minValue = 0.0f;
-        leftEyelidSlider.maxValue = 100.0f;
+        leftEyelidSlider.minValue  = 0.0f;
+        leftEyelidSlider.maxValue  = 100.0f;
         rightEyelidSlider.minValue = 0.0f;
         rightEyelidSlider.maxValue = 100.0f;
+
+        raiseEyebrowSlider.minValue = 0.0f;
+        raiseEyebrowSlider.maxValue = 100.0f;
+        angleEyebrowSlider.minValue = 0.0f;
+        angleEyebrowSlider.maxValue = 100.0f;
+        angleEyebrowSlider.value    = 50.0f;
+
+        mouthHSlider.minValue = 0.0f;
+        mouthHSlider.maxValue = 100.0f;
+        mouthVSlider.minValue = 0.0f;
+        mouthVSlider.maxValue = 100.0f;
 
         // Connect Signals
         leftEyelidSlider.onValueChanged.AddListener(OnLeftEyelidChanged);
         rightEyelidSlider.onValueChanged.AddListener(OnRightEyelidChanged);
+
+        raiseEyebrowSlider.onValueChanged.AddListener(OnRaiseEyebrowChanged);
+        angleEyebrowSlider.onValueChanged.AddListener(OnAngleEyebrowChanged);
+
+        mouthHSlider.onValueChanged.AddListener(OnMouthHChanged);
+        mouthVSlider.onValueChanged.AddListener(OnMouthVChanged);
     }
 
 }
