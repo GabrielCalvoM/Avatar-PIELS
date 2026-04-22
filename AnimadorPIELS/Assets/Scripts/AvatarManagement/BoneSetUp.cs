7bc8bd64f0d79cf66fa5917e415b9e73c7e54cf3
C-Espriella13 <cahucaes05@gmail.com>
2026-04-21 20:49:35 -0600

    Bone Nomenclature

---
+++ AnimadorPIELS/Assets/Scripts/AvatarManagement/BoneSetUp.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BoneSetUp : MonoBehaviour
+{
+
+    // Bone Nomenclature List
+    /*
+        Bone_Neck + NeckMov Script + NeckConstraint + FaceFocus[Camera]
+        Bone_UpperArm_<L/R> + UpperArmMov Script
+        Bone_ForeArm_<L/R> + LowerArmMov Script + ElbowConstraint
+        Bone_Hand_<L/R> + HandMove Script + HandConstraint + HandFocus[Camera]
+        Bone_F_Index_<L/R>_01 + Bone_F_Index_<L/R>_02 + FingerMov Script + FingerConstraint01 + FingerConstraint02
+        Bone_F_Middle_<L/R>_01 + Bone_F_Middle_<L/R>_02 + FingerMov Script + FingerConstraint01 + FingerConstraint02
+        Bone_F_Ring_<L/R>_01 + Bone_F_Ring_<L/R>_02 + FingerMov Script + FingerConstraint01 + FingerConstraint02
+        Bone_F_Pinky_<L/R>_01 + Bone_F_Pinky_<L/R>_02 + FingerMov Script + FingerConstraint01 + FingerConstraint02
+        Bone_F_Thumb_<L/R>_01 + Bone_F_Thumb_<L/R>_02 + FingerMov Script + FingerConstraint01 + FingerConstraint02        
+    */
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
